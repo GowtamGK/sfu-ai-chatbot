26962da4729fd72675e382628ef166e3ec1080f7
GowtamGK <138728679+GowtamGK@users.noreply.github.com>
2025-04-19 20:13:43 -0700

    Update news.js

--- news.js
+++ news.js
@@ -12,7 +12,8 @@ window.addEventListener("DOMContentLoaded", () => {
 //         "<p>Sorry, we could not load the latest news right now.</p>";
 //     });
 // });
-  fetch("https://sfu-ai-chatbot-production.up.railway.app/api/full-news")
+  //fetch("https://sfu-ai-chatbot-production.up.railway.app/api/full-news")
+   fetch("https://sfu-ai-chatbot-production-6037.up.railway.app/api/full-news")
     .then((res) => res.json())
     .then((data) => {
       document.getElementById("loading-spinner")?.remove(); // Remove spinner
